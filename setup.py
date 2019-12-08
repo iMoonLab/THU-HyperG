6d98b6e5f35bea74fad11058746fc31b6dc39d24
Haojie Lin <hjlin0515@gmail.com>
2019-12-08 20:45:29 +0800

    update requirements and setup

--- setup.py
+++ setup.py
@@ -21,5 +21,7 @@ setup(
     packages=find_packages(exclude=['test']),
     classifiers=[
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
